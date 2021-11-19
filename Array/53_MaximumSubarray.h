8e8d1bd431e9bb354ef04d0bf3d5eac8ffebe914
Paul Burgess <paulburgess1357@gmail.com>
2021-11-18 20:18:00 -0700

    Maximum subarray

---
+++ Array/53_MaximumSubarray.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+
+// https://leetcode.com/problems/maximum-subarray/
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// Difficulty: Easy
+
+namespace MaximumSubarray {
+
+    class Solution {
+    public:
+        // Time Complexity: O(n^2)
+		// Space Complexity: O(1)
+        int maxSubArray(std::vector<int>& nums) {
+            int maxSubArray = INT_MIN;
+            for(int i = 0; i < nums.size(); i++) {
+                int currentSubArray = 0;
+                for(int j = i; j < nums.size(); j++) {
+                    currentSubArray += nums[j];
+                    maxSubArray = std::max(maxSubArray, currentSubArray);
+                }
+            }
+            return maxSubArray;
+        }
+
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        // See: Kadane's Algorithm
+
+        int maxSubArray2(const std::vector<int>& nums) {
+            // Set Max and Current as first element:
+            int maxSubArray = nums.at(0);
+            int currentBestSubArrayUpToThisPoint = nums.at(0);
+            for(int i = 1; i < nums.size(); i++) {
+                currentBestSubArrayUpToThisPoint = std::max(nums.at(i), currentBestSubArrayUpToThisPoint + nums.at(i));
+                maxSubArray = std::max(currentBestSubArrayUpToThisPoint, maxSubArray);
+            }
+            return maxSubArray;
+        }
+        
+
+    };
+
+}
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questio
 #### 121.  [BestTimeToBuyAndSellStock](https://github.com/paulburgess1357/Blind75/blob/master/Array/121_BestTimeToBuyAndSellStock.h)
 #### 217.  [ContainsDuplicate](https://github.com/paulburgess1357/Blind75/blob/master/Array/217_ContainsDuplicate.h)
 #### 238.  [ProductOfArrayExceptSelf](https://github.com/paulburgess1357/Blind75/blob/master/Array/238_ProductOfArrayExceptSelf.h)
+#### 53.  [MaximumSubarray](https://github.com/paulburgess1357/Blind75/blob/master/Array/53_MaximumSubarray.h)
+
+53_MaximumSubarray.h
